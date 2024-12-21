25ed34571eb0082abbeaeb374ee72975300c2382
Flavio <flavio088gomes@gmail.com>
2024-12-20 22:56:19 -0300

    classes

--- Fundamentos Javascript/objetos/objetos2.js
+++ Fundamentos Javascript/objetos/objetos2.js
@@ -1,4 +1,25 @@
-let nome = "prop"
-console.log(nome)
+function Manga(title, author, chapters) {
+    this.title = title;
+    this.author = author;
+    this.chapters = chapters;
+    this.publish = false;
+    this.inStock = 0;
+    this.addtoStock = function addtoStock(quantity) {
+        this.inStock += quantity
+    }
+    this.save = function save() {
 
-let objeto = {  }
+    }
+}
+
+const OnePiece = new Manga("One Piece", "Eiichiro Oda", 1000);
+
+const Naruto = new Manga("Naruto", "Masashi Kishimoto", 1000);
+
+const Bleach = new Manga("Bleach", "Tite Kubo", 1000);
+
+console.log(OnePiece);
+
+console.log(Naruto);
+
+console.log(Bleach);
